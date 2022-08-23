2a8a4137491c4100716adf26e71f49fc99ed8bc2
piesieomane <piesieyeboah@gmail.com>
2022-08-23 18:00:16 +0300

    render table head correctly

--- src/components/pages/Mission.js
+++ src/components/pages/Mission.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import JoinMission from './JoinMission';
 
 const Mission = (props) => {
@@ -6,16 +7,27 @@ const Mission = (props) => {
   console.log('props', mission);
   return (
     <tr>
-      <h5>missionname</h5>
-
-      <p>missionmissiondescription</p>
-
-      <div className="align-middle">switchBadge</div>
-      <div className="align-middle">
-        <JoinMission />
-      </div>
+      <th>
+        <h5>{mission.mission_name}</h5>
+      </th>
+      <th>
+        <p>{mission.mission_description}</p>
+      </th>
+      <th className="align-middle">{'switchBadge'}</th>
+      <th className="align-middle">
+        <JoinMission isJoined={mission.isJoined} id={mission.mission_id} />
+      </th>
     </tr>
   );
+};
+
+Mission.propTypes = {
+  mission: PropTypes.shape({
+    mission_id: PropTypes.string,
+    mission_name: PropTypes.string,
+    mission_description: PropTypes.string,
+    isJoined: PropTypes.bool,
+  }).isRequired,
 };
 
 export default Mission;
