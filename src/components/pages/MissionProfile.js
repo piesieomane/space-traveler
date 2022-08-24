3781c71aebcca1578c5fd185066c0aa9a8c4c7b6
piesieomane <piesieyeboah@gmail.com>
2022-08-24 19:39:29 +0300

    display reserved missions in my profile

--- src/components/pages/MissionProfile.js
+++ src/components/pages/MissionProfile.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MissionProfile = () => <div>MissionProfile</div>;
+const MissionProfile = () => {
+  const missionProfile = useSelector((state) => state.missions);
+  // filter out the missions that are reserved
+  const reservedMissions = missionProfile.filter((mission) => mission.isJoined);
+  const displayMissions = reservedMissions.map((mission) => (
+    <tr key={mission.mission_id}>
+      <h2>{mission.mission_name}</h2>
+    </tr>
+  ));
+
+  return (
+    <div>
+      <h1>My Missions</h1>
+      <table>
+        <tbody>{displayMissions}</tbody>
+      </table>
+    </div>
+  );
+};
 
 export default MissionProfile;
