55e6657ea502b9d6c541cf5320d19517aeb98bd4
piesieomane <piesieyeboah@gmail.com>
2022-08-24 14:33:19 +0300

    fetch data from roctet API

---
+++ src/redux/rockets/rockets.js
@@ -0,0 +1,30 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const GET_ROCKETS = 'MISSIONS/GET_ROCKETS';
+
+const initialState = [];
+
+export const getROckets = createAsyncThunk(GET_ROCKETS, async () => {
+  const fetchAPI = await fetch('https://api.spacexdata.com/v3/rockets');
+  const data = await fetchAPI.json();
+  const rockets = data.map((rocket) => ({
+    id: rocket.id,
+    rocket_name: rocket.rocket_name,
+    description: rocket.description,
+    flickr_images: rocket.flickr_images[0],
+  }));
+  return {
+    rockets,
+  };
+});
+
+const rocketReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ROCKETS:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default rocketReducer;
