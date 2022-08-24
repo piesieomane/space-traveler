9361a09965b593a22b51f8e5097e4aec95cfb69e
piesieomane <piesieyeboah@gmail.com>
2022-08-24 12:24:17 +0300

    set fnuctionality for the join mission

--- src/components/pages/JoinMission.js
+++ src/components/pages/JoinMission.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { joinMission } from '../../redux/missions/missions';
 
-const JoinMission = () => <div>JoinMission</div>;
+const JoinMission = (props) => {
+  const { isJoined, id } = props;
+  const dispatch = useDispatch();
+
+  const clickButton = () => {
+    dispatch(joinMission(id));
+  };
+  return (
+    <div>
+      <button type="button" onClick={clickButton}>
+        {isJoined ? 'Leave Mission' : 'Join Mission'}
+      </button>
+    </div>
+  );
+};
+
+JoinMission.propTypes = {
+  isJoined: PropTypes.bool.isRequired,
+  id: PropTypes.string.isRequired,
+};
 
 export default JoinMission;
